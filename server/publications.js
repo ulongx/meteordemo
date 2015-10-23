62081c805f2c018004e084e1d948a22dce0398b4
Chuck <41q4@163.com>
2015-10-23 23:08:54 +0800

    add comments

--- server/publications.js
+++ server/publications.js
@@ -2,3 +2,8 @@
 Meteor.publish('posts', function(author) {
   return Posts.find();
 });
+//发布的评论，根据传过来的参数，获得评论
+Meteor.publish('comments', function(postId) {
+  check(postId, String);
+  return Comments.find({postId: postId});
+});
