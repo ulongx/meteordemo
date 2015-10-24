db0249b05bacfecc2a8ff9a8f3fd572de55401b3
Chuck <41q4@163.com>
2015-10-24 10:03:04 +0800

    login ui setLanguage('zh-CN');

--- server/fixtures.js
+++ server/fixtures.js
@@ -55,9 +55,20 @@ if (Posts.find().count() === 0) {
   Posts.insert({
     title: 'The Meteor Book',
     userId: ulongx._id,
-    author: tom.profile.name,
+    author: ulongx.profile.name,
     url: 'http://themeteorbook.com',
     submitted: new Date(now - 12 * 3600 * 1000),
     commentsCount: 0
   });
+
+  for (var i = 0; i < 10; i++) {
+    Posts.insert({
+      title: 'Test post #' + i,
+      author: ulongx.profile.name,
+      userId: ulongx._id,
+      url: 'http://google.com/?q=test-' + i,
+      submitted: new Date(now - i * 3600 * 1000),
+      commentsCount: 0
+    });
+  }
 }
